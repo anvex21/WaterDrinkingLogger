2e1befaefdccd01fbf50018ff4276192669ab546
anvex21 <alkinvejdi@gmail.com>
2024-08-07 23:27:23 +0300

    added the whole project

---
+++ WaterDrinkingLogger/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿function calculate() {
+    var tbl = document.getElementById("records");
+    var resultArea = document.getElementById("result");
+    var result = 0;
+    for (int i = 1; i < tbl.rows.length; i++)
+    {
+        result = result + +tbl.rows[i].cells[1].innerHTML;
+        
+    }
+    resultArea.append(result);
+}
